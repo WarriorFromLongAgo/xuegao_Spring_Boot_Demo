2a4c9b2456a0f563f1d5f4d7b2d317248373ba38
WarriorFromLongAgo <hubeixuegao@gmail.com>
2020-06-12 15:54:36 +0800

    测试

--- springboot2_3_rabbitmq/src/main/java/com/xuegao/springboot2_3_rabbitmq/Springboot23RabbitmqApplication.java
+++ springboot2_3_rabbitmq/src/main/java/com/xuegao/springboot2_3_rabbitmq/Springboot23RabbitmqApplication.java
@@ -1,10 +1,14 @@
 package com.xuegao.springboot2_3_rabbitmq;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Springboot23RabbitmqApplication {
+    
+    private static Logger log = LoggerFactory.getLogger(Springboot23RabbitmqApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Springboot23RabbitmqApplication.class, args);
