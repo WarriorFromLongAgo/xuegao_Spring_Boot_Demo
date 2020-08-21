e76783b15384e38c37cce9e268af7115b7cbd57c
费久猛(80004960) <hubeixuegao@gmail.com>
2020-08-21 20:10:13 +0800

    bean的加载

--- springboot_tool/src/main/java/com/xuegao/springboot_tool/config/KaptchaConfig.java
+++ springboot_tool/src/main/java/com/xuegao/springboot_tool/config/KaptchaConfig.java
@@ -1,7 +1,6 @@
 package com.xuegao.springboot_tool.config;
 
 import com.google.code.kaptcha.impl.DefaultKaptcha;
-import com.google.code.kaptcha.util.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,31 +16,31 @@ import java.util.Properties;
 @Configuration
 public class KaptchaConfig {
 
-    @Bean(name="captchaProducer")
-    public DefaultKaptcha getDefaultKaptcha(){
-        DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
-        Properties properties = new Properties();
-        // 图片边框
-        properties.setProperty("kaptcha.border", "no");
-        //去除干扰线
-        properties.setProperty("kaptcha.noise.impl", "com.google.code.kaptcha.impl.NoNoise");
-        // 边框颜色
-        properties.setProperty("kaptcha.border.color", "105,179,90");
-        // 字体颜色
-        properties.setProperty("kaptcha.textproducer.font.color", "black");
-        // 图片宽
-        properties.setProperty("kaptcha.image.width", "100");
-        // 图片高
-        properties.setProperty("kaptcha.image.height", "50");
-        // 字体大小
-        properties.setProperty("kaptcha.textproducer.font.size", "35");
-        // 验证码长度
-        properties.setProperty("kaptcha.textproducer.char.length", "4");
-        // 字体
-        properties.setProperty("kaptcha.textproducer.font.names", "宋体,楷体,微软雅黑");
-        Config config = new Config(properties);
-        defaultKaptcha.setConfig(config);
-
-        return defaultKaptcha;
-    }
+    // @Bean(name="captchaProducer")
+    // public DefaultKaptcha getDefaultKaptcha(){
+    //     DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
+    //     Properties properties = new Properties();
+    //     // 图片边框
+    //     properties.setProperty("kaptcha.border", "no");
+    //     //去除干扰线
+    //     properties.setProperty("kaptcha.noise.impl", "com.google.code.kaptcha.impl.NoNoise");
+    //     // 边框颜色
+    //     properties.setProperty("kaptcha.border.color", "105,179,90");
+    //     // 字体颜色
+    //     properties.setProperty("kaptcha.textproducer.font.color", "black");
+    //     // 图片宽
+    //     properties.setProperty("kaptcha.image.width", "100");
+    //     // 图片高
+    //     properties.setProperty("kaptcha.image.height", "50");
+    //     // 字体大小
+    //     properties.setProperty("kaptcha.textproducer.font.size", "35");
+    //     // 验证码长度
+    //     properties.setProperty("kaptcha.textproducer.char.length", "4");
+    //     // 字体
+    //     properties.setProperty("kaptcha.textproducer.font.names", "宋体,楷体,微软雅黑");
+    //     Config config = new Config(properties);
+    //     defaultKaptcha.setConfig(config);
+    //
+    //     return defaultKaptcha;
+    // }
 }
