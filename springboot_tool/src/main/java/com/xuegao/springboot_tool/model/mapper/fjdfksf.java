bd57ee41c1ab346a1f19b2a9060dbbbda17982cd
费久猛(80004960) <hubeixuegao@gmail.com>
2020-08-30 12:03:17 +0800

    mapsturct 的目录

---
+++ springboot_tool/src/main/java/com/xuegao/springboot_tool/model/mapper/fjdfksf.java
@@ -0,0 +1,12 @@
+package com.xuegao.springboot_tool.model.mapper;
+
+/**
+ * <br/> @PackageName：com.xuegao.springboot_tool.model.mapper
+ * <br/> @ClassName：fjdfksf
+ * <br/> @Description：
+ * <br/> @author：花名 xuegao
+ * <br/> @date：2020/8/30 12:02
+ */
+public class fjdfksf {
+    // mapsturct 的目录
+}
