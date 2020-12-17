87a1d3d4c32697f8d0c69c7df244ee8596199de1
80004960 <80004960@sfmail.sf-express.com>
2020-12-17 19:44:42 +0800

    BeanFactoryPostProcessor

--- springboot_tool/src/main/java/com/xuegao/springboot_tool/service/impl/SysUserinfoServiceImpl.java
+++ springboot_tool/src/main/java/com/xuegao/springboot_tool/service/impl/SysUserinfoServiceImpl.java
@@ -1,16 +1,12 @@
 package com.xuegao.springboot_tool.service.impl;
 
-import com.alibaba.fastjson.JSONObject;
-import com.baomidou.mybatisplus.core.metadata.OrderItem;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xuegao.springboot_tool.dao.SysUserinfoMapper;
 import com.xuegao.springboot_tool.model.PageInfo;
 import com.xuegao.springboot_tool.model.PageQuery;
 import com.xuegao.springboot_tool.model.doo.SysUserinfo;
 import com.xuegao.springboot_tool.service.interfaces.ISysUserinfoService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 /**
@@ -22,12 +18,12 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SysUserinfoServiceImpl extends ServiceImpl<SysUserinfoMapper, SysUserinfo> implements ISysUserinfoService {
-    private Logger log = LoggerFactory.getLogger(SysUserinfoServiceImpl.class);
 
     @Override
     public PageInfo<SysUserinfo> page2(PageQuery<SysUserinfo> pageQuery) {
         PageInfo<SysUserinfo> page = baseMapper.page2(pageQuery);
-        log.info(JSONObject.toJSONString(page));
+        String s = JSON.toJSONString(page);
+        LOG.info(s);
         return page;
     }
 
