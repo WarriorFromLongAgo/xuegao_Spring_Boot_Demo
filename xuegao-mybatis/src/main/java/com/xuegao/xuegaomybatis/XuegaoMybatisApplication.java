65d39dac491a04fe95f89d1038c9166b4f4d0f94
WarriorFromLongAgo <hubeixuegao@gmail.com>
2021-04-17 18:18:04 +0800

    init mybatis

---
+++ xuegao-mybatis/src/main/java/com/xuegao/xuegaomybatis/XuegaoMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.xuegao.xuegaomybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuegaoMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuegaoMybatisApplication.class, args);
+    }
+
+}
---
+++ xuegao-mybatis/src/test/java/com/xuegao/xuegaomybatis/XuegaoMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuegao.xuegaomybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuegaoMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
